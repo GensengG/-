e50103c682963822a514c2a86e06b702b3dbf512
GensengG <168531024+GensengG@users.noreply.github.com>
2024-12-29 20:42:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-// import { useState } from 'react'
-// import Stars from '../components/Stars.tsx'
-// import { Star } from '../components/Star.tsx'
-
 import Listing from '../components/Listing.tsx';
 import etsy from './etsy.json'
 import './App.css'
@@ -12,7 +8,6 @@ function App() {
   return (
       <div>
       <Listing data={items}/>
-      {/* {console.log(items)} */}
       </div>
   );
 }
